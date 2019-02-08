36dd46c9de1ae26e640862c06c29ccd4278d9d7c
Paulo Olayres <pjolayres@live.com>
2019-02-08 13:49:44 +0400

    Installed react-native-vector-icons via podfile and gradle

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { store, persistor } from './state/configureStore';
 import Main from './screens/main';
 import { AppContainer } from './navigation';
+
+Icon.loadFont();
 
 const App: React.FunctionComponent = () => {
   return (
